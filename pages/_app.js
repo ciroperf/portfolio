1a720fa512a135ead944b3f520c12a53dc546ec9
ciroperf <ciroperfetto@hotmail.it>
2025-05-30 22:03:55 +0200

    Update Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,58 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 
-const App = ({ Component, pageProps }) => {
+const GA_ID = "G-1F70620QDZ";
+
+function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag === "function") {
+        window.gtag("config", GA_ID, {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      {/* Load gtag.js */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+
+      {/* Inline script AFTER gtag is loaded */}
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
-};
+}
 
 export default App;
